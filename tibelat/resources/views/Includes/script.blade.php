480acec461559e08c351737286cd669a5b2c4c12
rimas12 <rizky_mashudy@yahoo.co.id>
2022-02-02 16:52:29 +0700

    admin integration laravel

---
+++ tibelat/resources/views/Includes/script.blade.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core JavaScript-->
+<script src="{{ url('BackEnd/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ url('BackEnd/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ url('BackEnd/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ url('BackEnd/js/sb-admin-2.min.js') }}"></script>
+
+<!-- Page level plugins -->
+<script src="{{ url('BackEnd/vendor/chart.js/Chart.min.js') }}"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{ url('BackEnd/js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ url('BackEnd/js/demo/chart-pie-demo.js') }}"></script>
---
+++ tibelat/resources/views/Includes/sidebar.blade.php
@@ -0,0 +1,62 @@
+<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-text mx-3">Tibelat Farm Admin</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-file-invoice-dollar"></i>
+            <span>Transaksi</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Jenis transaksi:</h6>
+                <a class="collapse-item" href="buttons.html">COD</a>
+                <a class="collapse-item" href="cards.html">Transfer</a>
+            </div>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-boxes"></i>
+            <span>Etalase</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-user-friends"></i>
+            <span>Customers</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-file-alt"></i>
+            <span>Laporan penjualan</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
